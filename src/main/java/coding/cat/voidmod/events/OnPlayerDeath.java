a07c485f24302b76f24aa6f626f76368a60354e1
JacksGalexite <jacksgalexite@gmail.com>
2026-09-15 11:40:04 -0700

    added a 30 second thing

--- src/main/java/coding/cat/voidmod/events/OnPlayerDeath.java
+++ src/main/java/coding/cat/voidmod/events/OnPlayerDeath.java
@@ -4,18 +4,62 @@ import coding.cat.voidmod.component.VoidComponents;
 import coding.cat.voidmod.component.VoidPlayerComponent;
 import coding.cat.voidmod.item.VoidItems;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-public class OnPlayerDeath implements ServerPlayerEvents.AllowDeath{
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class OnPlayerDeath implements ServerPlayerEvents.AllowDeath {
+
+    private static final int COUNTDOWN_TICKS = 600;
+    private static final Map<UUID, Integer> countdowns = new HashMap<>();
+
+    public static void register() {
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            countdowns.replaceAll((uuid, ticks) -> ticks - 1);
+
+            countdowns.entrySet().removeIf(entry -> {
+                if (entry.getValue() <= 0) {
+                    ServerPlayerEntity player =
+                            server.getPlayerManager().getPlayer(entry.getKey());
+
+                    if (player != null) {
+                        VoidPlayerComponent component =
+                                (VoidPlayerComponent) player.getComponent(VoidComponents.PLAYER);
+
+                        if (component.inLayer) {
+                            component.inLayer = false;
+                        }
+                    }
+
+                    return true;
+                }
+
+                return false;
+            });
+        });
+    }
+
     @Override
     public boolean allowDeath(ServerPlayerEntity player, DamageSource damageSource, float v) {
         if (player.getInventory().contains(new ItemStack(VoidItems.NULL_TOTEM))) {
+
             player.setHealth(player.getMaxHealth());
-            if (!((VoidPlayerComponent)player.getComponent(VoidComponents.PLAYER)).inLayer) {
-                ((VoidPlayerComponent)player.getComponent(VoidComponents.PLAYER)).inLayer = true;
+
+            VoidPlayerComponent component =
+                    (VoidPlayerComponent) player.getComponent(VoidComponents.PLAYER);
+
+            if (!component.inLayer) {
+                component.inLayer = true;
+
+                // Start the 30 second countdown
+                countdowns.put(player.getUuid(), COUNTDOWN_TICKS);
             }
+
             return false;
         }
 
